2efda36ccf4fe8c635b73c6d45d821dd73592d53
kiporovmatvey-netizen <kiporovmatvey@gmail.com>
2026-02-07 13:48:00 +0300

    Update espscript.lua

--- espscript.lua
+++ espscript.lua
@@ -1,62 +1,5 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Window = Rayfield:CreateWindow({
-   Name = "K.A.T SCRIPT!🔪",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Loading script..",
-   LoadingSubtitle = "by union shell hub",
-   ShowText = "K.A.T SCRIPT🔪", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "P", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = true, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "union shell hub"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Hello! Get ur key can get here!",
-      Subtitle = "Key System",
-      Note = "https://discord.gg/cWQzhYT8", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"UNIONSHELLHUBBEST"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
-
-local MainTab = Window:CreateTab("MAIN🏡") -- Title, Image
-
-local Section = MainTab:CreateSection("MAIN🏡")
-Rayfield:Notify({
-   Title = "executed",
-   Content = "thanks for useing",
-   Duration = 6.5
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Aimbot🔫 (XENO AND SOLARA SUPPORTED!)",
-   Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ReWelp/ChairHub./refs/heads/main/universalaimbot.lua", true))()
-   end,
-})
-
-local Button = Tab:CreateButton({
-   Name = "Button Example",
-   Callback = function()
-        local Players = game:GetService("Players")
+local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local RunService = game:GetService("RunService")
 
@@ -137,5 +80,3 @@ RunService.Heartbeat:Connect(function()
         end
     end
 end)
-   end,
-})
